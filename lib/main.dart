3ee788a5dcb0805ec5588ceb3913565b42caa944
Moataz Elgazzar <moataz.mahmoad@gmail.com>
2025-10-08 01:33:14 +0300

    Update readme

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,13 @@ import 'package:bookia/core/routes/routes.dart';
 import 'package:bookia/core/services/dio/dio_provider.dart';
 import 'package:bookia/core/services/local/shered_preferences.dart';
 import 'package:bookia/core/utils/theme.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DioProvider.init();
   await SheredPreferences.init();
-  runApp(DevicePreview(enabled: !kReleaseMode, builder: (BuildContext context) => const MainApp()));
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
